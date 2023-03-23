086676602c9751b7430bd52e8140fa8ad745bfc0
Jeremy School <jschool@Jeremys-MacBook-Pro.local>
2023-03-23 01:51:08 -0400

    disable every minute jobs to find restart loop issue

--- app/context.ts
+++ app/context.ts
@@ -209,14 +209,14 @@ export default class Context {
         })
 
         // retrieve quotes
-        scheduler.add({
-            runOnStart: true,
-            interval: EVERY_MINUTE,
-            function: async () => {
-                const response = await this.quoteService.fetchQuotes(this.settings.DefaultSymbols)
-                console.log(response)
-            }
-        })
+        // scheduler.add({
+        //     runOnStart: true,
+        //     interval: EVERY_MINUTE,
+        //     function: async () => {
+        //         const response = await this.quoteService.fetchQuotes(this.settings.DefaultSymbols)
+        //         console.log(response)
+        //     }
+        // })
 
         // price history
         scheduler.add({
@@ -239,14 +239,14 @@ export default class Context {
         })
 
         // send recommendations out
-        scheduler.add({
-            runOnStart: true,
-            interval: EVERY_MINUTE,
-            function: async () => {
-                const response = await this.recommendationService.processRecommendations()
-                console.log(response)
-            }
-        })
+        // scheduler.add({
+        //     runOnStart: true,
+        //     interval: EVERY_MINUTE,
+        //     function: async () => {
+        //         const response = await this.recommendationService.processRecommendations()
+        //         console.log(response)
+        //     }
+        // })
 
         return scheduler
     }
